52942bacbf40412a739d9eafec955c89a1913c2f
Habiba mohammad <88598540+Habiba-Mohammad@users.noreply.github.com>
2023-03-08 23:54:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base:"/Milestone",
+//   resolve: { extensions: [], }
+// })
 export default defineConfig({
   plugins: [react()],
-  base:"/Milestone",
-  resolve: { extensions: [], }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
