942a4b32f18bdd48bbff0e05b61814edd4a76e8e
wedding fashion <hm9461978@gmail.com>
2023-04-01 00:18:26 +0200

    fixed

--- src/Components/Help/Help.jsx
+++ src/Components/Help/Help.jsx
@@ -7,9 +7,6 @@ const Help = () => {
     <h1 className="help-title"> هل واجهت مشكلة اثناء تقييم طفلك ؟</h1>
     <p>يمكنك التوجه إلى أخصائي نطق ولغة للاطمئنان على طفلك </p>
    <div>
-    <p > برجاء التواصل معنا عبر الإيميل :<span> mhabibamohamad@gmail.com</span></p>
-   <p> التواصل عبر linkedin: <span><a href="https://www.linkedin.com/in/habiba-mohammad-a98703223/" > حبيبة محمد مصطفى </a></span> </p>
-   {/* <p> التواصل عبر الهاتف : </p> */}
    </div>
    </div>
    <img className='doctor' src={doctor} alt="doctor"/>
