afea0851f64dd0a173504ef52309e4f8644d66c3
wedding fashion <hm9461978@gmail.com>
2023-05-08 19:28:43 +0300

    accurate nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import "./Nav.css"
-import { Link } from 'react-router-dom'
+import { Link, Router } from 'react-router-dom'
 const Nav = () => {
   return (
 <>
@@ -9,22 +9,24 @@ const Nav = () => {
 <label className='menu-button-container' htmlFor="menu-toggle">
     <div className='menu-button'></div>
   </label>
-           <ul className='menu'>
-          
-             <li> <Link className="link"to="/">الصفحة الرئيسية </Link></li>
-              {/* <Link className="link" to="/login">تسجيل الدخول</Link> */}
-
-             <li> <Link className="link" to="/English">English</Link></li>
-              <li><Link className="link" to="/about">تعرف علينا </Link></li>
-            <li> <Link className="link" to="/article">مقالات أخرى </Link></li>
-             <li> <Link className="link" to="/help">تحتاج إلى مساعدة </Link></li>
-
-          </ul>
-
+  
 <div className='logo'>
   <p className='logoName'> طفلي</p>
   <i className='iconName'></i>
 </div>
+           <ul className='menu'>
+
+             <li> <Link className="link"to="/" >الصفحة الرئيسية </Link></li>
+              {/* <Link className="link" to="/login">تسجيل الدخول</Link> */}
+              <li> <Link className="link" to='Arabic'>عربي</Link></li>
+
+             <li> <Link className="link" to='English'>English</Link></li>
+              <li><Link className="link" to="about">تعرف علينا </Link></li>
+            <li> <Link className="link" to="article">مقالات أخرى </Link></li>
+             <li> <Link className="link" to="help">تحتاج إلى مساعدة </Link></li>
+
+          </ul>
+
 </nav>
 
 
